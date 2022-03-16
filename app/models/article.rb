bf6218a89f5aa125944dc8d1e9d675c2c8fd980c
Toufik El bouamri <elb.toufik@gmail.com>
2022-03-16 16:43:27 +0100

    refactored and created partials for form new and edit and private methods for find @article and article_params in articlesController

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 6, maximum: 25 }, uniqueness: { case_sensitive: false }
   validates :description, presence: true,
-                          length: { minimum: 1, maximum: 50 },
+                          length: { minimum: 1, maximum: 350 },
                           uniqueness: { case_sensitive: false }
 end
